6d112bc8b82b01e827a1838c77078c10d2d6dd3c
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-03-21 14:10:49 -0600

    Added routes

--- api/src/controllers/index.controller.js
+++ api/src/controllers/index.controller.js
@@ -1,7 +1,53 @@
-const getUsers = (req, res) => {
-    res.send('GET users callback');
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: '',
+    database: 'postmp3',
+    port: '5432'
+});
+
+const getUsers = async (req, res) => {
+    const response = await pool.query('SELECT * FROM users').rows;
+    res.json(response);
+};
+
+const getUserByUsername = async(req, res) => {
+    const response = await pool.query("SELECT * FROM usuario WHERE username='$1'", [req.params.username]).rows;
+    res.json(response);
+}
+
+const addUser = async (req, res) => {
+    const {username, password} = req.body;
+    const response = await pool.query('INSER INTO user VALUES ($1, $2)', [username, password]);
+    res.json({
+        message: 'User added',
+        body: {
+            user: {username,password}
+        }
+    })
+};
+
+const getSongs = async (req, res) => {
+    const response = await pool.query("SELECT * FROM song").rows;
+    res.json(response);
+};
+
+const getSongsByName = async(req, res) => {
+    const response = await pool.query("SELECT * FROM song WHERE nombre='$1'", [req.params.name]).rows;
+    res.json(response);
+};
+
+const getSearch = async (req, res) => {
+    //var querySong = 'SELECT '
 }
 
 module.exports = {
-    getUsers
-}
+    getUsers,
+    getUserByUsername,
+    addUser, 
+    getSongs,
+    getSongsByName,
+    getSearch
+};
--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -1,8 +1,20 @@
 const {Router} = require('express');
 const router = Router();
 
-const {getUsers} = require('../controllers/index.controller');
+const {getUsers, getUserByUsername, addUser, 
+    getSongs, getSongsByName,
+    getSearch} = require('../controllers/index.controller');
 
 router.get('/users', getUsers);
+router.get('/users/:username', getUserByUsername)
+
+router.get('/songs', getSongs);
+router.get('/songs/:name', getSongsByName);
+
+router.get('/search/:input', getSearch);
+
+
+router.post('/users', addUser);
+
 
 module.exports = router;
