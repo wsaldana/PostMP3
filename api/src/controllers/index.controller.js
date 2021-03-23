d0e303120c654d70a2bae2218ea721dfacd33280
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-03-23 17:05:34 -0600

    Added endpoints

--- api/src/controllers/index.controller.js
+++ api/src/controllers/index.controller.js
@@ -8,8 +8,9 @@ const pool = new Pool({
     port: '5432'
 });
 
+/** USERS ------------------------------------------------------------------------------------------------------------------------------------*/
 const getUsers = async (req, res) => {
-    const response = await pool.query('SELECT * FROM users').rows;
+    const response = await pool.query('SELECT * FROM usuario').rows;
     res.json(response);
 };
 
@@ -29,19 +30,45 @@ const addUser = async (req, res) => {
     })
 };
 
+/** SONGS ------------------------------------------------------------------------------------------------------------------------------------*/
 const getSongs = async (req, res) => {
     const response = await pool.query("SELECT * FROM song").rows;
     res.json(response);
 };
 
 const getSongsByName = async(req, res) => {
-    const response = await pool.query("SELECT * FROM song WHERE nombre='$1'", [req.params.name]).rows;
+    const response = await pool.query("SELECT * FROM song WHERE nombre='$1' AND enabled=true", [req.params.name]).rows;
     res.json(response);
 };
 
 const getSearchSongs = async (req, res) => {
-    //const response = await pool.query("SELECT * FROM song WHERE LOWER(nombre) LIKE '%$1%' OR LOWER()", [req.params.name]).rows;
+    const response = await pool.query("SELECT * FROM song s INNER JOIN artist a ON s.id_artist = a.id_artist WHERE LOWER(s.song_name) LIKE '%$1%' OR LOWER(a.artist_name) LIKE '%$1%' AND s.enabled = true", [req.params.name]).rows;
+    res.json(response);
 }
+
+const getSearchSongsByGenre = async (req, res) => {
+    const response = await pool.query("SELECT * FROM song WHERE genre = '$1'", [req.params.genre]).rows;
+    res.json(response);
+}
+
+/** ARTISTA ------------------------------------------------------------------------------------------------------------------------------------*/
+const getArtists = async (req, res) => {
+    const response = await pool.query("SELECT * FROM artist").rows;
+    res.json(response);
+}
+
+const getArtistByName = async (req, res) => {
+    const response = await pool.query("SELECT * FROM artist WHERE LOWER(art_name) LIKE '%$1%'", [req.params.name]).rows;
+    res.json(response);
+}
+
+const addArtist = async (req, res) => {
+    const {id, name, } = req.body;
+}
+
+/** ALBUMES ------------------------------------------------------------------------------------------------------------------------------------*/
+
+
 
 module.exports = {
     getUsers,
