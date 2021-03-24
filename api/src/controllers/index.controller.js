3b8b604492b7a102a6b14551faf72c2ebfb2172b
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-03-23 23:24:25 -0600

    fix routes

--- api/src/controllers/index.controller.js
+++ api/src/controllers/index.controller.js
@@ -3,25 +3,31 @@ const {Pool} = require('pg');
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
-    password: '',
+    password: 'gMzQQ2M7J2SD',
     database: 'postmp3',
     port: '5432'
 });
 
+const getLogin = async (req, res) => {
+    const {username, password} = req.params
+    const response = await pool.query("SELECT id_user, username FROM Usuario WHERE username=$1 AND contrasena=$2", [username, password]);
+    res.json(response.rows[0]);
+}
+
 /** USERS ------------------------------------------------------------------------------------------------------------------------------------*/
 const getUsers = async (req, res) => {
-    const response = await pool.query('SELECT * FROM usuario').rows;
-    res.json(response);
+    const response = await pool.query('SELECT * FROM Usuario');
+    res.json({rows: response.rows});
 };
 
 const getUserByUsername = async(req, res) => {
-    const response = await pool.query("SELECT * FROM usuario WHERE username='$1'", [req.params.username]).rows;
-    res.json(response);
+    const response = await pool.query("SELECT * FROM usuario WHERE username=$1", [req.params.username]);
+    res.json(response.rows[0]);
 }
 
 const addUser = async (req, res) => {
-    const {username, password} = req.body;
-    const response = await pool.query('INSER INTO user VALUES ($1, $2)', [username, password]);
+    const {username, password} = req.query;
+    const response = await pool.query("INSERT INTO Usuario(username,contrasena) VALUES ($1, $2)", [username, password]);
     res.json({
         message: 'User added',
         body: {
@@ -32,18 +38,18 @@ const addUser = async (req, res) => {
 
 /** SONGS ------------------------------------------------------------------------------------------------------------------------------------*/
 const getSongs = async (req, res) => {
-    const response = await pool.query("SELECT * FROM song").rows;
-    res.json(response);
+    const response = await pool.query("SELECT * FROM song");
+    res.json({rows: response.rows});
 };
 
 const getSongsByName = async(req, res) => {
-    const response = await pool.query("SELECT * FROM song WHERE nombre='$1' AND enabled=true", [req.params.name]).rows;
-    res.json(response);
+    const response = await pool.query("SELECT * FROM Song WHERE song_name=$1 AND enabled=true", [req.params.name]);
+    res.json(response.rows[0]);
 };
 
 const getSearchSongs = async (req, res) => {
-    const response = await pool.query("SELECT * FROM song s INNER JOIN artist a ON s.id_artist = a.id_artist WHERE LOWER(s.song_name) LIKE '%$1%' OR LOWER(a.artist_name) LIKE '%$1%' AND s.enabled = true", [req.params.name]).rows;
-    res.json(response);
+    const response = await pool.query("SELECT * FROM song s INNER JOIN artist a ON s.id_artist = a.id_artist WHERE LOWER(s.song_name) LIKE LOWER('%"+req.params.input+"%') OR LOWER(a.art_name) LIKE LOWER('%"+req.params.input+"%') AND s.enabled = true");
+    res.json({rows: response.rows});
 }
 
 const getSearchSongsByGenre = async (req, res) => {
@@ -71,6 +77,7 @@ const addArtist = async (req, res) => {
 
 
 module.exports = {
+    getLogin,
     getUsers,
     getUserByUsername,
     addUser, 
--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
 const router = Router();
 
-const {getUsers, getUserByUsername, addUser, 
+const {getLogin, getUsers, getUserByUsername, addUser, 
     getSongs, getSongsByName,
     getSearchSongs} = require('../controllers/index.controller');
+
+router.get('/login/:username/:password', getLogin)
 
 router.get('/users', getUsers);
 router.get('/users/:username', getUserByUsername)
